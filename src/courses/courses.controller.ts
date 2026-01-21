6b3a0e2ab25dd338c59cd6cf185507e103b8a7ac
Yusuf Sunnatullayev <yusuffdeveloper@gmail.com>
2026-01-22 00:39:35 +0500

    fix

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -9,31 +9,37 @@ import {
 } from '@nestjs/common';
 import { CoursesService } from './courses.service';
 import { CourseDto } from './dto/course.dto';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('courses')
 export class CoursesController {
   constructor(private readonly coursesService: CoursesService) {}
 
+  @ApiBearerAuth('access-token')
   @Post()
   create(@Body() dto: CourseDto) {
     return this.coursesService.create(dto);
   }
 
+  @ApiBearerAuth('access-token')
   @Get()
   findAll() {
     return this.coursesService.findAll();
   }
 
+  @ApiBearerAuth('access-token')
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.coursesService.findOne(id);
   }
 
+  @ApiBearerAuth('access-token')
   @Patch(':id')
   update(@Param('id') id: string, @Body() dto: CourseDto) {
     return this.coursesService.update(id, dto);
   }
 
+  @ApiBearerAuth('access-token')
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.coursesService.remove(id);
--- src/lessons/lessons.controller.ts
+++ src/lessons/lessons.controller.ts
@@ -9,31 +9,37 @@ import {
 } from '@nestjs/common';
 import { LessonsService } from './lessons.service';
 import { LessonDto } from './dto/lesson.dto';
+import { ApiBearerAuth } from '@nestjs/swagger';
 
 @Controller('lessons')
 export class LessonsController {
   constructor(private readonly lessonsService: LessonsService) {}
 
+  @ApiBearerAuth('access-token')
   @Post()
   create(@Body() dto: LessonDto) {
     return this.lessonsService.create(dto);
   }
 
+  @ApiBearerAuth('access-token')
   @Get()
   findAll() {
     return this.lessonsService.findAll();
   }
 
+  @ApiBearerAuth('access-token')
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.lessonsService.findOne(id);
   }
 
+  @ApiBearerAuth('access-token')
   @Patch(':id')
   update(@Param('id') id: string, @Body() dto: Partial<LessonDto>) {
     return this.lessonsService.update(id, dto);
   }
 
+  @ApiBearerAuth('access-token')
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.lessonsService.remove(id);
