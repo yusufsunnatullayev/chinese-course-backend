e6be93bca82b8e7120ed30b24ac6206bc18b8e8d
Yusuf Sunnatullayev <yusuffdeveloper@gmail.com>
2026-05-15 21:53:21 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -58,8 +58,7 @@ async function bootstrap() {
   );
 
   // Listen on Railway port
-  const port = process.env.PORT ?? 3000;
+  const port = parseInt(process.env.PORT ?? '3000', 10);
   await app.listen(port, '0.0.0.0');
-  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
